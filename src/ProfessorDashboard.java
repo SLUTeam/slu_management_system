ff433a5a141b1f1f0397bda81b99873a054e176b
abishek3358 <abishek582000@gmail.com>
2025-08-07 18:57:45 -0500

    Git clean

--- src/ProfessorDashboard.java
+++ src/ProfessorDashboard.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
-//Developed by Abishek Arivudainambi
+//Developed by Abishek Arivudainambi 
 
 public class ProfessorDashboard extends JFrame {
 
